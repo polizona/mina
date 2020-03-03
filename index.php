d6128d3ecf2d2fbe104483cc52944bdccdda68bb
profegil <gramirezfl@ipn.mx>
2020-03-03 11:18:13 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<Title>Clasificación de Clientes</Title>	
+	</head>
+	<body>
+		<form action="funcion.php" method="get">
+			
+			<label>Quiero Clasificar a mis clientes</label><br><br>
+			
+			Selecciona la industria a la cual perteneces:
+			<select name="opciones">
+				<option value="A"> A</option>
+				<option value="B"> B</option>
+				<option value="C"> C</option>
+				<option value="D"> D</option>
+				<option value="E"> E</option>
+				<option value="F"> F</option>
+			</select><br><br>
+			
+			<input type="submit"/>
+		</form>
+	</body>
+</html>
