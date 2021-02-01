f3394c224f1298716aa948658b5902adc56c35f2
profegil <profe.gilberto.ramirez@gmail.com>
2021-02-01 03:28:31 -0600

    Add files via upload

---
+++ recupera.php
@@ -0,0 +1,36 @@
+<?PHP
+	$hostname_localhost ="68.70.164.26";
+	$database_localhost ="polizona_uno";
+	$username_localhost ="polizona_profe";
+	$password_localhost ="Poli-profe3";
+	$json=array();
+				
+	$conexion = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
+	$consulta="select * from alcaldia;";
+		$resultado=mysqli_query($conexion,$consulta);
+		if($conexion){
+			echo "<table>";
+			echo "<th> idalcaldia</th>";
+			echo "<th> alcaldia</th>";
+			while($registro=mysqli_fetch_array($resultado)){
+				$result["idalcaldia"]=$registro['idalcaldia'];
+				$result["alcaldia"]=$registro['alcaldia'];
+				$json['Clasificador'][]=$result;
+				
+				echo "<tr>";
+					echo "<td>".$registro['idalcaldia']." </td>";
+					echo "<td>".$registro['alcaldia']." </td>";
+				echo "</tr>";
+			}
+			
+			echo "</table>";
+			
+			json_encode($json);
+			mysqli_close($conexion);
+		}
+		else{
+			echo "error";
+		}
+		
+			
+	?>
